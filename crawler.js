50c5ce8b7d6b0b5e183f792d9f7ca58da36f2749
sl1mSha4dey <mutenssj@gmail.com>
2022-10-05 21:13:09 +0200

    fixed selectors not found issue

--- crawler.js
+++ crawler.js
@@ -1,125 +1,149 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const schedule = require('node-schedule');
 
-const rule = new schedule.RecurrenceRule();
-rule.hour = 21;
-rule.minute = 52;
+(async () => {
 
-const job = schedule.scheduleJob(rule, async function () {
-	(async () => {
-		console.log('Launching browser...');
+	const last_run_time = fs.readFileSync(process.env.TIME_FILE, { encoding: 'utf8' });
 
-		const browser = await puppeteer.launch({ headless: false });
-		let page = await browser.newPage();
+	let current_date = new Date();
+	const current_date_str = `${current_date.getDay()}/${current_date.getMonth()}/${current_date.getFullYear()}`;
 
-		await page.goto('https://www.instagram.com/');
+	if (last_run_time === current_date_str) return;
+	
+	console.log('Launching browser...');
 
-		console.log('Opening Instagram...');
+	const browser = await puppeteer.launch({ headless: true });
+	let page = await browser.newPage();
 
-		console.log('Accepting Cookies (boriiiing)...');
+	await page.goto('https://www.instagram.com/');
 
-		const accept_cookies_btn = await page.$('button.aOOlW.HoLwm');
+	console.log('Opening Instagram...');
 
-		await accept_cookies_btn.click();
+	console.log('Accepting Cookies (boriiiing)...');
 
-		console.log('Cookies accepeted!');
+	const accept_cookies_btn = await page.$('button.aOOlW.HoLwm');
 
-		// Sleep for 2 seconds -> wait for the page to finish loading
-		console.log('Waiting for the page to finish loading...');
-		await sleep();
+	await accept_cookies_btn.click();
 
-		console.log('Searching for the username and password input...');
+	console.log('Cookies accepeted!');
 
-		const username_input = await page.$(
-			'#loginForm input[name="username"]'
-		);
-		const password_input = await page.$(
-			'#loginForm input[name="password"]'
-		);
+	// Sleep for 2 seconds -> wait for the page to finish loading
+	console.log('Waiting for the page to finish loading...');
+	await sleep();
 
-		console.log('Logging in...');
+	console.log('Searching for the username and password input...');
 
-		await username_input.type(process.env.IG_USR);
-		await password_input.type(process.env.IG_PWD);
+	const username_input = await page.$('#loginForm input[name="username"]');
+	const password_input = await page.$('#loginForm input[name="password"]');
 
-		const login_btn = await page.$('#loginForm button[type="submit"]');
-		login_btn.click();
+	console.log('Logging in...');
 
-		await page.waitForSelector(
+	await username_input.type(process.env.IG_USR);
+	await password_input.type(process.env.IG_PWD);
+
+	const login_btn = await page.$('#loginForm button[type="submit"]');
+	login_btn.click();
+
+	await page
+		.waitForSelector(
 			'div._ab8j._ab8s._ab8w._ab94._ab99._ab9f._ab9m._ab9p._ac8g._abcm'
-		);
+		)
+		.catch((err) => {
+			console.error('Selector could not be found: Save Information Page');
+		});
 
-		const save_info_btn = await page.$(
+	const save_info_btn = await page
+		.$(
 			'div._ab8j._ab8s._ab8w._ab94._ab99._ab9f._ab9m._ab9p._ac8g._abcm div._ac8f > button'
-		);
-		save_info_btn.click();
-
-		await page.waitForSelector('button._a9--._a9_1');
-
-		console.log('Logged in!');
-
-		//Disable notification
-		const notification_btn = await page.$('button._a9--._a9_1');
-		notification_btn.click();
-
-		await sleep();
-
-		const upload_preview_btn = await page.$(
-			"._acub > button._abl-._abm2[type='button'] > ._abm0"
-		);
-		await upload_preview_btn.click();
-
-		const upload_file_btn = await page.$(
-			'div._ab8w._ab94._ab99._ab9f._ab9m._ab9p._ab9x._aba7._abcm > button._acan._acap._acas'
-		);
-
-		const [fileChooser] = await Promise.all([
-			page.waitForFileChooser(),
-			upload_file_btn.click(),
-		]);
-
-		console.log('Uploading file...');
-
-		await fileChooser.accept([process.env.PHOTO_PATH]);
-
-		await sleep(3000);
-
-		for (let i = 0; i < 2; i++) {
-			const next_btn = await page.$(
-				'div[role="dialog"] button._acan._acao._acas'
+		)
+		.catch((err) => {
+			console.error(
+				'Selector could not be found: Save Information Button'
 			);
+		});
 
-			await next_btn.click();
-			await sleep(1000);
-		}
+	if (save_info_btn != null) save_info_btn.click();
 
-		const caption = await page.$('div[role="dialog"] textarea._ablz._aaeg');
+	await page.waitForSelector('button._a9--._a9_1').catch((err) => {
+		console.error('Selector could not be found: Notification Page');
+	});
 
-		const data = fs.readFileSync('time.txt', { encoding: 'utf8' });
+	console.log('Logged in!');
 
-		const str = `Day ${parseInt(data) + 1} of 365`;
+	//Disable notification
+	const notification_btn = await page.$('button._a9--._a9_1').catch((err) => {
+		console.error('Selector could not be found: Notification Button');
+	});
 
-		await caption.type(str);
+	if (notification_btn != null) notification_btn.click();
 
-		console.log('Sharing post...');
+	await page.waitForSelector(
+		"._acub > button._abl-._abm2[type='button'] > ._abm0"
+	);
 
-		const share_btn = await page.$(
+	const upload_preview_btn = await page.$(
+		"._acub > button._abl-._abm2[type='button'] > ._abm0"
+	);
+
+	await upload_preview_btn.click();
+
+	await page.waitForSelector(
+		'div[role="dialog"] button._acan._acap._acas'
+	);
+
+	const upload_file_btn = await page.$(
+		'div[role="dialog"] button._acan._acap._acas'
+	);
+
+	const [fileChooser] = await Promise.all([
+		page.waitForFileChooser(),
+		upload_file_btn.click(),
+	]);
+
+	console.log('Uploading file...');
+
+	await fileChooser.accept([process.env.PHOTO_PATH]);
+
+	await sleep(5000);
+
+	for (let i = 0; i < 2; i++) {
+		const next_btn = await page.$(
 			'div[role="dialog"] button._acan._acao._acas'
 		);
 
-		await share_btn.click();
+		await next_btn.click();
+		await sleep(1000);
+	}
 
-		await sleep(10000);
+	await page.waitForSelector('div[role="dialog"] textarea._ablz._aaeg');
 
-		fs.writeFileSync(process.env.FILE_PATH, `${parseInt(data) + 1}`);
+	const caption = await page.$('div[role="dialog"] textarea._ablz._aaeg');
 
-		console.log('All done!');
+	const data = fs.readFileSync(process.env.DAYS_FILE, { encoding: 'utf8' });
 
-		await browser.close();
-	})();
-});
+	const str = `Day ${parseInt(data) + 1} of 365`;
+
+	await caption.type(str);
+
+	console.log('Sharing post...');
+
+	const share_btn = await page.$(
+		'div[role="dialog"] button._acan._acao._acas'
+	);
+
+	await share_btn.click();
+
+	await sleep(10000);
+
+	fs.writeFileSync(process.env.DAYS_FILE, `${parseInt(data) + 1}`);
+	current_date = new Date();
+	fs.writeFileSync(process.env.TIME_FILE, `${current_date.getDay()}/${current_date.getMonth()}/${current_date.getFullYear()}`);
+
+	console.log('All done!');
+
+	await browser.close();
+})();
 
 async function sleep(ms = 2000) {
 	return await new Promise((resolve) => setTimeout(resolve, ms));
